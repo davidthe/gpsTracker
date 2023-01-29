e0e36d8b49ab40acc02d9b59eac360853c7690ae
David Mizrchi <david@aka.co.il>
2023-01-29 02:43:14 +0200

    cesium map working

---
+++ src/app/cesium.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import { SceneMode, Viewer } from 'cesium';
+
+@Directive({
+  selector: '[appCesium]'
+})
+export class CesiumDirective implements OnInit {
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    const viewer = new Viewer(this.el.nativeElement, {
+      homeButton: false,
+      sceneMode: SceneMode.SCENE2D,
+      timeline:false,
+      automaticallyTrackDataSourceClocks: false,
+      animation: false,
+      navigationHelpButton: false,
+      fullscreenButton: false,
+      vrButton: false,
+    });
+  }
+
+}
